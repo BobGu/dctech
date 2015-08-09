34b7f4006c4d95a2f51910eba433fbcc51aefb50
bobgu <robertgu84@gmail.com>
2015-08-09 15:56:56 -0400

    Posts flickr pictures from the past month
    The initial page load is a tad bit longer than what I want it to be.
    Also the React code is becoming a little bit gross looking.
    
    I want to refactor and be able to speed up the initial load time.  Will
    do research and try to implement.
    
    Also I need a piece that lets people know which pictures are rank the
    highest by comments.

--- src/dctech.js
+++ src/dctech.js
@@ -20,8 +20,51 @@ var PictureBox = React.createClass({
             return instagramObject.created_time > thirtyDaysAgo;
           });
           allInstagramObjects = allInstagramObjects.concat(filtered_data);
-          var sorted_data = _.sortBy(allInstagramObjects, function(instagramObject){
-            return [instagramObject.comments.count, parseInt(instagramObject.created_time)];
+          allInstagramObjects = _.map(allInstagramObjects, function(instagramObject) {
+            instagramObject.url = instagramObject.images.standard_resolution.url;
+            return instagramObject;
+          });
+
+          var flickrObjects;
+          var thirtyDaysAgo = (Date.now() / 1000) - (60 * 60 * 24 * 30);
+          $.ajax({
+            url:  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=013a3a4a679d634b6f2114ceb152a22e&tags=dctech&min_upload_date=" + thirtyDaysAgo / 10 + "&format=json&nojsoncallback=1",
+            dataType: 'json',
+            cache: false,
+            async: false,
+            complete: function(xhr, textStatus) {
+              if (xhr.status == 200) {
+                flickrObjects = JSON.parse(xhr.responseText).photos.photo;
+                // make another ajax request to the photos.comments.getlist
+                if(flickrObjects.length > 0 ) {
+                  flickrObjects = _.map(flickrObjects, function(flickrObject) {
+                    $.ajax({
+                      url: "https://api.flickr.com/services/rest/?method=flickr.photos.comments.getList&api_key=013a3a4a679d634b6f2114ceb152a22e&photo_id=" + flickrObject.id +  "&format=json&nojsoncallback=1",
+                      cache: false,
+                      async: false,
+                      complete: function(xhr, textStatus) {
+                        if(JSON.parse(xhr.responseText).comments.length === undefined) {
+                          var numberOfComments = 0;
+                          flickrObject.comments = {count: numberOfComments};
+                        }
+                        else {
+                          var numberOfComments = JSON.prase(xhr.responseText).comments;
+                          flickrObject.comments = {count: numberOfComments};
+                        }
+                        flickrObject.url = "https://farm" + flickrObject.farm + ".staticflickr.com/" + flickrObject.server +  "/" + flickrObject.id + "_" + flickrObject.secret +  ".jpg";
+                      }
+                    });
+                    return flickrObject;
+                  });
+                }
+              } else {
+                console.log(xhr.status)
+              }
+            },
+          });
+          var allObjects = flickrObjects.concat(allInstagramObjects);
+          var sorted_data = _.sortBy(allObjects, function(object){
+            return object.comments.count;
           });
           sorted_data = sorted_data.reverse();
           this.setState({data: sorted_data });
@@ -48,9 +91,9 @@ var PictureBox = React.createClass({
 });
 var Pictures = React.createClass({
   render: function() {
-    var pictureNodes = this.props.data.map(function (instagramObject) {
+    var pictureNodes = this.props.data.map(function (object) {
       return (
-        <Picture url={instagramObject.images.standard_resolution.url} />
+        <Picture url={object.url} />
       );
     });
     return (
@@ -77,5 +120,3 @@ React.render(
   <PictureBox url='https://api.instagram.com/v1/tags/dctech/media/recent?client_id=fcd848752dbb44bebe0143378aa2142c' />,
   document.getElementById("content")
 );
-
-flikr api key 013a3a4a679d634b6f2114ceb152a22e
